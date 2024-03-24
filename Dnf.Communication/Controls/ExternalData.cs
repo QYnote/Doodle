f3e00c6b0dc7abd79858cfc905643259980324f9
QYnote <jmhc6@naver.com>
2024-03-24 14:19:17 +0900

    통신 파일 Util -> Communication Project로 이동

---
+++ Dnf.Communication/Controls/ExternalData.cs
@@ -0,0 +1,50 @@
+﻿using Dnf.Communication.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Dnf.Communication.Controls
+{
+    //외부 데이터 연동
+    public class ExternalData
+    {
+        public string FilePath = RuntimeData.DataPath;
+
+        #region XML
+
+        public string XmlLoad_ModelStruct(UnitModel model, string attribute)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            try
+            {
+                string path = string.Format("{0}\\{1}.xml", FilePath, "ModelStruct");
+
+                if (!System.IO.File.Exists(path)) { throw new Exception(RuntimeData.String("XmlLoad ModelStruct Error")); }
+
+                xdoc.Load(path);
+                XmlElement root = xdoc.DocumentElement;
+
+                foreach (XmlNode node in root.ChildNodes)
+                {
+                    if (node.Attributes["Name"].Value != model.ToString()) continue;
+
+                    return node.Attributes[attribute].Value;
+                }
+
+                return null;
+            }
+            catch {
+                return null;
+            }
+        }
+
+        #endregion XML End
+
+        #region JSON
+
+        #endregion
+    }
+}
